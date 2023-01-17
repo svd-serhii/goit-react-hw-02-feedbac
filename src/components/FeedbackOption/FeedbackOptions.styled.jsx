c8971c275a0c44f855b291133565538c59d0eb30
Serhii Demchenko <svdemchenko@gmail.com>
2023-01-17 20:01:52 +0200

    Upd feedback styles

--- src/components/FeedbackOption/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOption/FeedbackOptions.styled.jsx
@@ -7,17 +7,18 @@ export const FeedbackList = styled.ul`
 `;
 export const FeedbackItem = styled.li``;
 export const FeedbackBtn = styled.button`
+  text-transform: capitalize;
   width: 100px;
   font-size: 16px;
   font-weight: 400;
   color: black;
-  padding: 10px;
+  padding: 5px 10px;
+  border: 1px solid grey;
   border-radius: 4px 4px 4px 4px;
   cursor: pointer;
   transition: scale 300ms ease-in-out;
   outline: none;
-  border: none;
-  :hover {
+  &:hover {
     scale: 1.2;
   }
   :active {
